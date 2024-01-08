5eb158a1be2e0c97b97792cbc4393da6201b3372
Sid Thomas <thomas.siddharth@gmail.com>
2024-01-08 12:44:53 -0800

    [docs] Formatting and small low-level documentation

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-/* 
+/*
  * Developed by Siddharth Thomas '2025
- * 
+ *
  * Logs the elapsed time between check in and check out by week, with a 30 minute timeout
  * Takes data from a spreadsheet's form responses in the format: [timestamp, member ID, input("In" or "Out")]
  * Creates a custom menu for users from the "admins" list to check a member in or out or modify a member's most recent week's time
  * Requires a sheet called "Result Sheet" to store data in, with a sheet-wide filter view
- * 
+ *
  * Run triggers: onFormSubmit, onOpen
  */
 
@@ -40,7 +40,7 @@ function checkOut(id, elapsed, modifier) {
     let day = weekStart.getDay() || 7;
     weekStart.setHours(-24 * (day-1));
     weekStart.setMinutes(0,0,0);
-    
+
     // Create a column headed by the Monday's date and filled with empty times
     resultSheet.insertColumnBefore(5);
     resultSheet.getRange('E1').setValue(weekStart);
@@ -55,14 +55,14 @@ function checkOut(id, elapsed, modifier) {
   // Create date object from time
   const [hours, minutes, seconds] = time.split(':');
   time = new Date(Date.UTC(1970, 0, hours/24 + 1, hours%24, minutes, seconds));
-  
+
   // Add elapsed time to date object, multiplying by the modifier if given
   modifier = modifier || 1;
   // Stop if time or change is invalid
   if (time.toString() === 'Invalid Date') {throw 'Invalid starting time';}
   if (modifier < 0 && time.getTime() + elapsed*modifier < 0) {throw 'Cannot remove more hours than are logged';}
   time.setTime(time.getTime() + elapsed*modifier);
-  
+
   // Send string from date object to spreadsheet
   time = `${(time.getUTCDate()-1)*24 + time.getUTCHours()}:${time.getUTCMinutes()}:${time.getUTCSeconds()}`;
   resultSheet.getRange(members.indexOf(id)+2, 5).setValue(time);
@@ -79,7 +79,7 @@ function checkForTimeout() {
       checkOut(member, 1800000);
       // Remove check in data to avoid double check-outs
       delete checkedIn[member];
-      
+
       // Add to the number of timeouts
       resultSheet.getRange(members.indexOf(member)+2, 4).setValue(resultSheet.getRange(members.indexOf(member)+2, 4).getValue() + 1);
     }
@@ -95,7 +95,7 @@ function addNewMember(id) {
     resultSheet.getRange(2,4).setValue(0);
     resultSheet.getRange(2, 5).setValue('0:0:0');
     resultSheet.getFilter().sort(2, false);
-    
+
     // Update member list to account for change in spreadsheet
     members.push(member);
   }
@@ -108,7 +108,7 @@ function onFormSubmit(e) {
   updateVars();
   addNewMember(id);
   checkForTimeout();
-  
+
   // If inputting to the form and has been checked in, check out
   if (checkedIn.hasOwnProperty(id)) {checkOut(id, timestamp - checkedIn[id]);}
   // If checking in, add timestamp to sheet
@@ -132,11 +132,11 @@ function modifyHours() {
   updateVars();
   const ui = SpreadsheetApp.getUi();
   const id = inputMember();
-  
+
   // Recieve and validate a time input
   const timeResponse = ui.prompt('Amend Hours', id + '\nTime modifier (h:m:s)', ui.ButtonSet.OK_CANCEL);
   if (timeResponse.getSelectedButton() != ui.Button.OK) {throw 'Operation canceled';}
-  
+
   // Create date and modifier from time input
   var elapsed = timeResponse.getResponseText();
   var modifier = 1;
@@ -167,7 +167,7 @@ function adminCheckIn(t) {
 
   if (checkedIn.hasOwnProperty(id)) {checkOut(id, time - checkedIn[id]);}
   resultSheet.getRange(members.indexOf(id)+2, 3).setValue(new Date(time));
-  
+
   const ui = SpreadsheetApp.getUi();
   ui.alert('Confirmation', `${id} checked in`, ui.ButtonSet.OK);
 }
@@ -178,12 +178,12 @@ function adminCheckOut() {
   const id = inputMember();
   if (!checkedIn.hasOwnProperty(id)) {throw `${id} is not checked in`;}
   checkOut(id, Date.now() - checkedIn[id].getTime());
-  
+
   const ui = SpreadsheetApp.getUi();
   ui.alert('Confirmation', `${id} checked out`, ui.ButtonSet.OK);
 }
 
-// Checks in every member already checked in
+// Re-check in every member already checked in, effectively resetting the timeout counter
 function timeoutReset() {
   updateVars();
   var updatedMembers = [];
@@ -193,7 +193,7 @@ function timeoutReset() {
     resultSheet.getRange(members.indexOf(id)+2, 3).setValue(new Date(time));
     updatedMembers.push(id);
   }
-  
+
   const ui = SpreadsheetApp.getUi();
   const message = updatedMembers.length === 0 ? 'No checked in members' : (updatedMembers.length === 1 ? updatedMembers[0] + ' has been checked in' : updatedMembers.join(',\n') + '\nhave been checked in');
   ui.alert('Confirmation', message, ui.ButtonSet.OK);
@@ -205,6 +205,7 @@ function adminTimeout() {
   checkForTimeout();
 }
 
+// Initialize admin menu for sheet editors
 function onOpen(e) {
   // Stop if user's email does not match an editor
   if (!resultSheet.getEditors().find(editor => e.user.getEmail() === editor.getEmail())) {return;}
